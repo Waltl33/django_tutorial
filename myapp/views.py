fcdc819047966471470edc10db8689cbcb1194c9
Walter <walter.clark24@yahoo.com>
2023-10-27 05:39:00 -0400

    sending dynamic data

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    '''
+    name = "Patrick"
+    '''
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationality': 'British'
+
+    }
+    '''
+    return render(request, 'index.html', {'name': name})
+    '''
+    return render(request, 'index.html', context)
+    
