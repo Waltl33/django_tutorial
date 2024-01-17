552a193f4bd6c7f14d2813c87603bc800953e286
Walter <walter.clark24@yahoo.com>
2024-01-17 10:04:03 -0500

    dynamic table

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
-    is_true: bool
+class Feature(models.Model):
+    name = models.CharField(max_length=100, default = '')
+    details = models.CharField(max_length = 500, default = '')
+    
--- myapp/views.py
+++ myapp/views.py
@@ -14,31 +14,8 @@ def index(request):
     }
     
     '''
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = "Fast"
-    feature1.is_true = True
-    feature1.details = 'Our service is very quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = "Reliable"
-    feature2.is_true = True
-    feature2.details = 'Our service is very reliable'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = "Affordable"
-    feature3.is_true = False
-    feature3.details = 'Our service is very affordable'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = "Easy to Use"
-    feature4.is_true = True
-    feature4.details = 'Our service is very easy to use'
-
-    features = [feature1, feature2, feature3, feature4]
+ 
+    features = Feature.objects.all
     
     return render(request, 'index.html', {'features': features})
     
